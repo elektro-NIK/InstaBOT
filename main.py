a6e6fbb716b7592249db76db54b52c44bf561986
elektroNIK <bogdan.kalinin@gmail.com>
2018-11-02 15:09:29 +0200

    Saving to and loading from file results Simplifying JSON

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import json
+import pickle
 
 from db import DB
 from key import login, password
@@ -47,10 +48,65 @@ def create_tables(db):
     ))
 
 
+def save2file(obj, filename):
+    with open(filename, 'wb') as file:
+        pickle.dump(obj, file)
+
+
+def load_from_file(filename):
+    with open(filename, 'rb') as file:
+        res = pickle.load(file)
+    return res
+
+
+def log(x):
+    pass    # Fixme!
+
+
+def simplify_history(history):
+    types = {12: 'comment', 60: 'like', 101: 'following'}
+    types_ignoring = {43: 'facebook friend'}
+    res = []
+    for story in history:
+        try:
+            type = story['story_type']
+        except KeyError:
+            log(story)
+            continue
+        if type in types:
+            s = {
+                'type': types[type],
+                'text': story['args']['text'],
+                'timestamp': story['args']['timestamp'],
+                'profile': {
+                    'id': story['args']['profile_id'],
+                    'name': story['args']['profile_name'],
+                    'image': story['args']['profile_image']
+                },
+                'link': {
+                    'id': story['args']['links'][0]['id'],
+                    'type': story['args']['links'][0]['type'],
+                    'start': story['args']['links'][0]['start'],
+                    'end': story['args']['links'][0]['end']
+                }
+            }
+            try:
+                s['media'] = story['args']['media'][0]['image']
+            except KeyError:
+                pass
+            res.append(s)
+        elif type in types_ignoring:
+            pass
+        else:
+            log(story)
+    return res
+
+
 if __name__ == '__main__':
     insta = API(login=login, password=password)
     last_activity = insta.login()
     history = last_activity['new_stories'] + last_activity['old_stories']
+    save2file(history, 'save_002')
     db = DB('instaBot.sqlite')
     db.connect()
     create_tables(db)
